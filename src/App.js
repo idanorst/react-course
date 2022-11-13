4000ba06d9b0c4da5033253ee7dbe38abbd20ab6
idanorst <111854278+idanorst@users.noreply.github.com>
2022-11-13 13:22:13 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Card from './components/Card'
+
+export default function App() {
+    return (
+        <div className="container">
+            <Card />
+        </div>
+    )
+}
