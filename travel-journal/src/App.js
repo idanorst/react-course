72d653a4a975851ee51ad99d2b10dafd4608f7dc
idanorst <111854278+idanorst@users.noreply.github.com>
2022-11-18 12:50:57 +0100

    Add files via upload

---
+++ travel-journal/src/App.js
@@ -0,0 +1,12 @@
+import './style.css';
+import Navbar from './components/Navbar'
+import Journal from './components/Journal'
+
+export default function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Journal />
+    </div>
+  );
+}
---
+++ travel-journal/src/data.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        title: "Stetind", 
+        location: "Norway",
+        googleMapsLink: "https://www.google.com/maps/place/Stetinden/@68.1651926,16.592763,14z/data=!3m1!4b1!4m5!3m4!1s0x45dbfc7a4db50e37:0x3fbb492c91a53f40!8m2!3d68.165193!4d16.592763",
+        startDate: "10 Jul 2023",
+        endDate: "12 Jul 2023",
+        description: "Stetind is an iconic mountain in the northern part of Norway. With it's height of 1392 meters the view from the top is amazing.",
+        imageUrl: "https://i.pinimg.com/originals/6d/91/d6/6d91d6387f65405766310124dd845c42.jpg"
+    },
+    {
+        title: "St. Moritz", 
+        location: "Switzerland",
+        googleMapsLink: "https://www.google.com/maps/place/St.+Moritz,+Sveits/@46.4861563,9.8069619,12z/data=!3m1!4b1!4m5!3m4!1s0x478482076dc01a7b:0x279fdbbd3ec97825!8m2!3d46.4907973!4d9.8355079",
+        startDate: "4 Aug 2023",
+        endDate: "10 Aug 2023",
+        description: "St. Moritz is a high alpine resort town in the Engadine in Switzerland, at an elevation of about 1,800 meters (5,910 ft) above sea level.",
+        imageUrl: "https://media.istockphoto.com/id/540504958/photo/lake-misurina-in-the-italian-dolomites-in-the-winter.jpg?s=612x612&w=0&k=20&c=MBrlAJiwPA-c5ur77GGygC9mWoBOYlEgaSl19gD6kf0="
+    },
+    {
+        title: "Cape Town", 
+        location: "South-Africa",
+        googleMapsLink: "https://www.google.com/maps/place/Cape+Town,+S%C3%B8r-Afrika/@-33.915538,18.6560594,9z/data=!3m1!4b1!4m5!3m4!1s0x1dcc500f8826eed7:0x687fe1fc2828aa87!8m2!3d-33.9248685!4d18.4240553",
+        startDate: "???",
+        endDate: "???",
+        description: "Cape Town is one of South Africa's three capital cities, serving as the seat of the Parliament of South Africa. It is legislative capital of the country, the oldest city in the country, and the second largest city. The city is know for landmarks such as the Table Mountain and Cape Point",
+        imageUrl: "https://media.istockphoto.com/id/620737858/photo/cape-town-and-the-12-apostels-from-above.jpg?s=612x612&w=0&k=20&c=WPP0CFtX4y-eHwplbZ1DvPP9bnGjjpz4U4ZZvj8i8Tc="
+    },
+    {
+        title: "Mount Fuji",
+        location: "Japan", 
+        googleMapsLink: "https://www.google.com/maps/place/Fuji-san/@35.3606247,138.7186086,15z/data=!4m5!3m4!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634",
+        startDate: "12 Jan 2023",
+        endDate: "24 Jan 2023",
+        description: "Mount Fuji is the tallest mountain in Japan, standing at 3,766 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.",
+        imageUrl: "https://images.unsplash.com/photo-1578001356991-159e54ec17ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80" 
+    },
+    {
+        title: "Sydney Opera House",
+        location: "Australia",
+        googleMapsLink: "https://www.google.com/maps/place/Operahuset+i+Sydney/@-33.8567799,151.213108,17z/data=!3m2!4b1!5s0x6b12ae67d234a27f:0xd6d4e9380ca1e32f!4m5!3m4!1s0x6b12ae665e892fdd:0x3133f8d75a1ac251!8m2!3d-33.8567844!4d151.2152967",
+        startDate: "27 May 2023",
+        endDate: "8 Jun 2023",
+        description: "The Sydney Opera House is a multi-venue performing arts centre in Sydney. Located on the banks of the Sydney Harbour, it is often regarded as one of the 20th century's most famous and distinctive buildings.",
+        imageUrl: "https://images.unsplash.com/photo-1595740229246-cfdda61917c6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=436&q=80"
+    },
+    {
+        title: "Geirangerfjord",
+        location: "Norway",
+        googleMapsLink: "https://www.google.com/maps/place/Geirangerfjorden/@62.1048487,7.0752131,11z/data=!4m5!3m4!1s0x46169d427b268c51:0xb8c99540dcc397fe!8m2!3d62.101506!4d7.0940816",
+        startDate: "01 Oct 2023",
+        endDate: "18 Nov 2023",
+        description: "The Geiranger Fjord is a fjord in the Sunnmøre region of Møre og Romsdal county, Norway. It is located entirely in the Stranda Municipality.",
+        imageUrl: "https://media.istockphoto.com/id/1140015027/photo/geirangerfjord-fjord-and-seven-sisters-waterfall.jpg?s=612x612&w=0&k=20&c=E2vcSxbuEd3zddNsU7Od13pJwmCBcvDqHLlfQSFmHRE="
+    }
+]
